8d2b52f08922fb55d822368ae8fd148d5ae23417
KIRAN PATIL <147366909+kiranpatil6663@users.noreply.github.com>
2025-09-08 19:39:26 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ export const ShopContext =createContext();
 const ShopContextProvider = (props)=>{
     const currency = '$';
     const delivery_fee=10;
-    const backendUrl=import.meta.env.VITE_BACKEND_URL
+    const backendUrl=https://e-commerce-if5p.onrender.com
     const [cartItems,setCartItems]=useState({});
     const [token,setToken]=useState('');
     const navigate=useNavigate();
